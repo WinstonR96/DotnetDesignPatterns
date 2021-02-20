ab5e83c8f85435758a8cfdeb8a8eca21cd95099b
Winston Jr Rodriguez Stand <wijurost@gmail.com>
2021-02-20 13:19:21 -0500

    Agregando estructura conceptual del flyweight

---
+++ Flyweight/Flyweight/Flyweight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+// Use Json.NET library,
+using Newtonsoft.Json;
+
+namespace Flyweight.Flyweight
+{
+    /*El Flyweight almacena una parte común del estado (también llamado estado intrínseco) 
+     * que pertenece a múltiples entidades comerciales reales. Flyweight acepta el resto del 
+     * estado (estado extrínseco, único para cada entidad) a través de sus parámetros de método.*/
+    public class Flyweight
+    {
+        private Car _sharedState;
+
+        public Flyweight(Car car)
+        {
+            this._sharedState = car;
+        }
+
+        public void Operation(Car uniqueState)
+        {
+            string s = JsonConvert.SerializeObject(this._sharedState);
+            string u = JsonConvert.SerializeObject(uniqueState);
+            Console.WriteLine($"Flyweight: Displaying shared {s} and unique {u} state.");
+        }
+    }
+}
---
+++ Flyweight/Program.cs
@@ -0,0 +1,59 @@
+﻿using Flyweight.Flyweight;
+using System;
+
+namespace Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //El código del cliente generalmente crea un montón de pesos mosca precargados en la 
+            //etapa de inicialización de la aplicación.
+            var factory = new FlyweightFactory(
+                new Car { Company = "Chevrolet", Model = "Camaro2018", Color = "pink" },
+                new Car { Company = "Mercedes Benz", Model = "C300", Color = "black" },
+                new Car { Company = "Mercedes Benz", Model = "C500", Color = "red" },
+                new Car { Company = "BMW", Model = "M5", Color = "red" },
+                new Car { Company = "BMW", Model = "X6", Color = "white" }
+            );
+            factory.listFlyweights();
+
+            addCarToPoliceDatabase(factory, new Car
+            {
+                Number = "CL234IR",
+                Owner = "James Doe",
+                Company = "BMW",
+                Model = "M5",
+                Color = "red"
+            });
+
+            addCarToPoliceDatabase(factory, new Car
+            {
+                Number = "CL234IR",
+                Owner = "James Doe",
+                Company = "BMW",
+                Model = "X1",
+                Color = "red"
+            });
+
+            factory.listFlyweights();
+        }
+
+        public static void addCarToPoliceDatabase(FlyweightFactory factory, Car car)
+        {
+            Console.WriteLine("\nClient: Adding a car to database.");
+
+            var flyweight = factory.GetFlyweight(new Car
+            {
+                Color = car.Color,
+                Model = car.Model,
+                Company = car.Company
+            });
+
+            // The client code either stores or calculates extrinsic state and
+            // passes it to the flyweight's methods.
+            flyweight.Operation(car);
+        }
+    }
+}
+
